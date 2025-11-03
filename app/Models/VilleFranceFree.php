a5388eaa40607e412cbb42f9cff6a72cd01dd137
GalakX2 <goupy.jordan@gmail.com>
2025-11-03 19:35:55 +0100

    Model / VilleFranceFree

---
+++ app/Models/VilleFranceFree.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VilleFranceFree extends Model
+{
+    use HasFactory;
+
+    // Nom exact de la table
+    protected $table = 'villes_france_free';
+
+    // Si ta table n’a pas de colonnes created_at / updated_at :
+    public $timestamps = false;
+
+    // Si ta clé primaire n’est pas "id", indique-la ici
+    // Par exemple, si c’est "ville_id"
+    protected $primaryKey = 'ville_id';
+
+    // Si la clé n’est pas auto-incrémentée :
+    // public $incrementing = false;
+
+    // Si la clé est une chaîne (pas un entier) :
+    // protected $keyType = 'string';
+
+    // Liste des colonnes modifiables (facultatif mais recommandé)
+    protected $fillable = [
+        'ville_id',
+        'ville_departement',
+        'ville_slug',
+        'ville_nom',
+        'ville_nom_simple',
+        'ville_nom_soundex',
+        'ville_nom_metaphone',
+        'ville_code_postal',
+        'ville_commune',
+        'ville_code_commune',
+        'ville_arrondissement',
+        'ville_canton',
+        'ville_amdi',
+        'ville_population_2010 ',
+        'ville_population_1999',
+        'ville_population_2012',
+        'ville_densite_2010',
+        'ville_surface',
+        'ville_longitude_deg',
+        'ville_latitude_deg',
+        'ville_longitude_grd',
+        'ville_latitude_grd',
+        'ville_longitude_dms',
+        'ville_latitude_dms',
+        'ville_zmin',
+        'ville_zmax'
+
+        // ajoute ici les noms exacts des colonnes de ta table
+    ];
+}
